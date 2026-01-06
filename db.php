b7ca8138ec7695e3c828271f40bc30349b404d4c
LeartSh <leart.shulina10@gmail.com>
2026-01-06 21:36:01 +0100

    Update db.php

--- db.php
+++ db.php
@@ -9,10 +9,9 @@ $conn = new mysqli($host, $username, $password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }else{
-header(location: "index.php");
+header("location: index.php");
 exit();
 }
 
 $conn->set_charset("utf8");
 ?>
-
